26710a7e0d846be6cab3470b8c1b70e683f782f6
aapostol <aapostol@student.42.fr>
2023-10-24 15:36:13 +0200

    Remove mlx

--- src/free_funcs.c
+++ src/free_funcs.c
@@ -2,22 +2,23 @@
 
 void	free_game(t_game *game)
 {
-	if (game->graphix.wall)
-		mlx_destroy_image(game->mlx_ptr, game->graphix.wall);
-	if (game->graphix.zero)
-		mlx_destroy_image(game->mlx_ptr, game->graphix.zero);
-	if (game->graphix.player)
-		mlx_destroy_image(game->mlx_ptr, game->graphix.player);
-	if (game->graphix.exit)
-		mlx_destroy_image(game->mlx_ptr, game->graphix.exit);
-	if (game->graphix.collectible)
-		mlx_destroy_image(game->mlx_ptr, game->graphix.collectible);
-	if (game->graphix.empty)
-		mlx_destroy_image(game->mlx_ptr, game->graphix.empty);
-	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
-	mlx_destroy_display(game->mlx_ptr);
-	game->mlx_ptr = free_ptr(game->mlx_ptr);
-	game->map = free_array(game->map);
+	(void)game;
+	// if (game->graphix.wall)
+	// 	mlx_destroy_image(game->mlx_ptr, game->graphix.wall);
+	// if (game->graphix.zero)
+	// 	mlx_destroy_image(game->mlx_ptr, game->graphix.zero);
+	// if (game->graphix.player)
+	// 	mlx_destroy_image(game->mlx_ptr, game->graphix.player);
+	// if (game->graphix.exit)
+	// 	mlx_destroy_image(game->mlx_ptr, game->graphix.exit);
+	// if (game->graphix.collectible)
+	// 	mlx_destroy_image(game->mlx_ptr, game->graphix.collectible);
+	// if (game->graphix.empty)
+	// 	mlx_destroy_image(game->mlx_ptr, game->graphix.empty);
+	// mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+	// mlx_destroy_display(game->mlx_ptr);
+	// game->mlx_ptr = free_ptr(game->mlx_ptr);
+	// game->map = free_array(game->map);
 }
 
 void	*free_array(char **arr)
